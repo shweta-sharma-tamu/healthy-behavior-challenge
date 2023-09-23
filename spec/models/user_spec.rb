e20318d2c7be93a6d596e4b6b6dc43edece4214f
Satya Bhavsar <satyabhavsar@tamu.edu>
2023-09-23 11:17:39 -0500

    Add tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,9 +3,21 @@ require "rails_helper"
 describe User, type: :model do
     describe "validations" do
         it "is invalid without an email" do
-            user = User.new
+            user = User.new(email:nil)
             expect(user).to be_invalid
         end
+         it "is invalid without a password" do
+                    user = User.new(email:nil)
+                    expect(user).to be_invalid
+         end
+          it "is invalid without a type" do
+                     user = User.new(user_type:nil)
+                     expect(user).to be_invalid
+          end
+           it "is invalid without an userid" do
+                      user = User.new(userid:nil)
+                      expect(user).to be_invalid
+           end
     end
 
 end
