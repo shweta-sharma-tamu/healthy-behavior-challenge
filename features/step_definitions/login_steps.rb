12454bea0188e70f421e22b6889c2c575d665f24
Satya Bhavsar <satyabhavsar@tamu.edu>
2023-09-23 12:39:11 -0500

    Add scenarios for login

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -2,14 +2,33 @@ Given(/^I am on the Login page$/) do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When('I enter admin@gmail.com as username') do
+When('I enter admin@gmail.com as email') do
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When('I enter Admin@{int} as password') do |int|
+When('I enter {string} as email') do |string|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
+When('I enter '' as email') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I enter Admin@123 as password') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I enter '' as password') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I enter {string} as password') do |string|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When('I enter abcd@123 as password') do
+  pending # Write code here that turns the phrase above into concrete actions
+end
 
 When('I click on submit') do
   pending # Write code here that turns the phrase above into concrete actions
