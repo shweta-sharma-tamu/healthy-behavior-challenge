d2063f9386195759ed201f3ed82be6603da35e80
Satya Bhavsar <satyabhavsar@tamu.edu>
2023-09-23 15:02:55 -0500

    Add initial implementation for login

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,26 @@
+class UsersController < ApplicationController
+
+    def new
+        @user=User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to root_path
+        else
+            render :new
+        end
+    end
+
+    def show
+        @user = User.find(params[:userid])
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :type, :userid)
+    end
+end
