a117f8bb5c3400fc613baf2991f6c14d9bc7d99d
shweta-sharma-tamu <143050103+shweta-sharma-tamu@users.noreply.github.com>
2023-09-24 18:06:25 -0500

    Added login logout feature

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,15 +12,19 @@ class SessionsController < ApplicationController
 
     if !!@user && @user.authenticate(params[:password])
         session[:user_id]=@user.id
-        flash[:notice] = "Successfully signed in"
+        flash[:notice] = "You have successfully signed in"
         redirect_to user_path(session[:user_id])
     else
         message = params[:email]
         message = "Incorrect email or password. Please try again."
         redirect_to new_session_url, notice: message
+    end
   end
 
   def destroy
+    session[:user_id] = nil # Clear the user's session
+    message = "You have been signed out."
+    redirect_to new_session_url, notice: message
   end
-end
+
 end
