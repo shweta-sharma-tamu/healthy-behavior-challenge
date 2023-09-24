9ffdd36ae73be09a9ea1829f37c9cf9aad67affd
Satya Bhavsar <satyabhavsar@tamu.edu>
2023-09-23 22:56:20 -0500

    Update cucumber tests for login

--- features/step_definitions/login_steps.rb
+++ features/step_definitions/login_steps.rb
@@ -1,41 +1,46 @@
-Given(/^I am on the Login page$/) do
-  pending # Write code here that turns the phrase above into concrete actions
+Given(/^I am not currently Logged in$/) do
+  visit login_path
+  expect(page).to have_content('Please Log in')
 end
 
-When('I enter admin@gmail.com as email') do
-  pending # Write code here that turns the phrase above into concrete actions
+Given(/^I am on the Login page$/) do
+  visit login_path
+end
+
+When('I enter Admin@gmail.com as email') do
+  fill_in 'email', with: 'Admin@gmail.com'
 end
 
 When('I enter {string} as email') do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  fill_in 'email', with: string
 end
 
 When('I enter '' as email') do
-  pending # Write code here that turns the phrase above into concrete actions
+  fill_in 'email', with: ''
 end
 
 When('I enter Admin@123 as password') do
-  pending # Write code here that turns the phrase above into concrete actions
+  fill_in 'password', with: 'Admin@123'
 end
 
 When('I enter '' as password') do
-  pending # Write code here that turns the phrase above into concrete actions
+  fill_in 'password', with: ''
 end
 
 When('I enter {string} as password') do |string|
-  pending # Write code here that turns the phrase above into concrete actions
+  fill_in 'password', with: string
 end
 
 When('I enter abcd@123 as password') do
-  pending # Write code here that turns the phrase above into concrete actions
+  fill_in 'password', with: 'abcd@123'
 end
 
 When('I click on submit') do
-  pending # Write code here that turns the phrase above into concrete actions
+  click_button 'Sign In!'
 end
 
 Then('I should see {string}') do |arg|
-  pending # Write code here that turns the phrase above into concrete actions
+  expect(page).to have_content(arg)
 end
 
 
