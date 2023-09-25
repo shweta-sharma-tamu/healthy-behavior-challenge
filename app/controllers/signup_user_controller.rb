c2efa2b0f3529668e5c40a0977668871e53c3d24
UtsavDabhi-tamu <utsav.dabhi@tamu.edu>
2023-09-25 07:34:52 -0500

    trainee signup controller added

---
+++ app/controllers/signup_user_controller.rb
@@ -0,0 +1,37 @@
+class SignupUserController < ApplicationController
+    def new
+        @user = User.new
+        @trainee = Trainee.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        @user.user_type = "trainee"
+        
+        @trainee = Trainee.new(trainee_params)
+
+        if @user.save
+
+            @trainee.user = @user
+            if @trainee.save
+                redirect_to root_path, notice: 'Signup successful!'
+            else
+                @user.destroy
+                render :new
+            end
+        else
+            render :new
+        end
+    end
+    
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation)
+    end
+    
+    def trainee_params
+        # params.require(:trainee).permit(:full_name, :height, :weight)
+        params.require(:user).permit(:full_name, :height, :weight)
+    end
+end
