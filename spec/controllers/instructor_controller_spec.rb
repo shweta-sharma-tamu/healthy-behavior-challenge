0bf92f6ea762c1c0ca8a9652f1593f6724d6103e
sharvaniramineni-tamu <sharvani.ramineni@tamu.edu>
2023-09-29 05:34:04 -0500

    fixed instructor_controller_spec.rb to fix build failure

--- spec/controllers/instructor_controller_spec.rb
+++ spec/controllers/instructor_controller_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe InstructorController, type: :controller do
   before :each do
     User.create(email: 'user1@gmail.com', password: 'password',user_type:"Instructor")
     @token = SecureRandom.uuid
-    user = User.find_by(email: "instructor@example.com")
+    @user = User.find_by(email: "instructor@example.com")
     # Create a new referral associated with the user
-    referral = user.instructor_referrals.create(email: "example@example.com", token: @token, is_used: false,expires: Date.today+7.days)
+    @referral = @user.instructor_referrals.create(email: "example@example.com", token: @token, is_used: false,expires: Date.today+7.days)
     @referral_link = "#{ENV['ROOT_URL']}#{instructor_signup_path(token: @token)}"
   end
 
