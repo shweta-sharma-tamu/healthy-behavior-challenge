9df3912779676dc295862ae38afadc7f0d1249e3
sharvaniramineni-tamu <sharvani.ramineni@tamu.edu>
2023-09-28 23:59:30 -0500

    seed changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,8 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-User.create(email: 'Admin@gmail.com', password: 'Admin@123', user_type: 'Instructor')
+user = User.create(email: 'Admin@gmail.com', password: 'Admin@123', user_type: 'Instructor')
+user_id = user.id
 User.create(email: 'Admin1@gmail.com', password: 'Admin1@123', user_type: 'Trainee')
+Instructor.create(user_id: user_id, first_name: 'Sheena', last_name: 'Sheena')
+
