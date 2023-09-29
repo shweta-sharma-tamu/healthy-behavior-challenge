eb97a45c216d5dc1c725e2561482bdac344bbcdb
sharvaniramineni-tamu <sharvani.ramineni@tamu.edu>
2023-09-29 05:01:40 -0500

    fix tests after instructor referral has merged

--- features/step_definitions/instructorsignup_steps.rb
+++ features/step_definitions/instructorsignup_steps.rb
@@ -1,7 +1,44 @@
-  Given(/^I am on instructor signup page$/) do
-    visit '/instructor_signup'
-    expect(page).to have_content('Sign up')
+  Given("I received email with referral link") do
+    @user = User.create!(email: 'instructor@example.com', password: 'abcdef',user_type: "Instructor")
+    puts @user.email
+    
+    visit login_path
+    expect(page).to have_content('User Sign in')
+    fill_in 'email', with: 'instructor@example.com'
+    fill_in 'password', with: 'abcdef'
+    click_button 'Sign In'
+    expect(page).to have_content("Welcome")
+    
+    # Implement the code to navigate to the referral page
+    visit instructor_referral_path
+    expect(page).to have_content("Please enter valid email to refer an instructor.")
+    
+    # Implement the code to fill in the recipient's email field with the provided email
+    @recipient_email = 'newuser@example.com'
+    fill_in 'email',with: 'newuser@example.com'
+    
+    # Implement the code to click the specified button
+    find("#refer-button").click
+    
+    # Implement the code to verify that a unique referral link was generated and sent to the recipient's email
+    puts @user.email
+    @token = InstructorReferral.where(email: @recipient_email).order(created_at: :desc).first.token
+    puts @token
+    expect(page).to have_content(@token)
+    
+    @token = SecureRandom.uuid
+    user = User.find_by(email: 'instructor@example.com')
+    # Create a new referral associated with the user
+    referral = user.instructor_referrals.create(email: "example@example.com", token: @token, is_used: false,expires: Date.today+7.days)
+    @referral_link = instructor_signup_path(token: @token)
+    puts @referral_link
   end
+
+  Given('I click the sent referral link and redirected to signup page') do
+  visit @referral_link
+  expect(page).to have_content('Sign up')
+  end
+  
   
   When('I fill in email with instructorsignup3@gmail.com') do
     fill_in 'Email', with: 'instructorsignup3@gmail.com'
@@ -58,10 +95,6 @@
   When("I click on signup") do
     click_button "Sign Up"
   end
-  
-  #Then('I should see {string}') do
-    #expect(page).to have_content("Welcome!, #{@first_name} #{@last_name}")
-  #end
 
   Then('I should see {string} message') do |msg|
     expect(page).to have_content(msg)
