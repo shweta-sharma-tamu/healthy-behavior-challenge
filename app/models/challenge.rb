d9e112c1220e30fc18e56b3aea836bbaff7e9bb3
shweta-sharma-tamu <143050103+shweta-sharma-tamu@users.noreply.github.com>
2023-10-09 15:54:49 -0500

    Fixed associations

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,6 +1,6 @@
 class Challenge < ApplicationRecord
   has_one :challenge_genericlist
   has_many :challenge_trainees
-  has_many :task, through: :challenge_genericlists
-  has_many :trainee, through: :challenge_trainees
+  has_many :tasks, through: :challenge_genericlist
+  has_many :trainees, through: :challenge_trainees
 end
--- app/models/trainee.rb
+++ app/models/trainee.rb
@@ -5,7 +5,7 @@ class Trainee < ApplicationRecord
     validates :height, presence: true, numericality: { greater_than: 0 }
     validates :weight, presence: true, numericality: { greater_than: 0 }
 
-    has_many :challenge_trainee
-    has_many :todolist_task
-    has_many :challenge, through: :challenge_trainee  
+    has_many :challenge_trainees
+    has_many :todolist_tasks
+    has_many :challenges, through: :challenge_trainees  
 end
