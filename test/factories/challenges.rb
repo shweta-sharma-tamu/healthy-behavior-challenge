dd8b5fc02e8f9afbb3bb449d106945655c480a47
shweta-sharma-tamu <143050103+shweta-sharma-tamu@users.noreply.github.com>
2023-10-13 12:52:13 -0500

    Removed unnecessary changes

--- test/factories/challenges.rb
+++ test/factories/challenges.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
-    factory :challenge do
-      Challenge_id { "" }
-      Name { "MyString" }
-      startDate { "2023-10-09" }
-      EndDate { "2023-10-09" }
-    end
+  factory :challenge do
+    Challenge_id { "" }
+    Name { "MyString" }
+    startDate { "2023-10-09" }
+    EndDate { "2023-10-09" }
   end
+end
