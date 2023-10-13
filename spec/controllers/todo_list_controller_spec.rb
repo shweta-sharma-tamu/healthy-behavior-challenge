4db630a420f0b776b6b33098560791e8fdeaf78e
sharvaniramineni-tamu <sharvani.ramineni@tamu.edu>
2023-10-13 15:35:08 -0500

    fixed rspec issue

--- spec/controllers/todo_list_controller_spec.rb
+++ spec/controllers/todo_list_controller_spec.rb
@@ -22,7 +22,7 @@ RSpec.describe TodoListController, type: :controller do
   describe 'GET #show' do
     it 'renders the show template' do
       get :show
-      expect(response).to render_template(todo_list_path(@user.id))
+      expect(response).to render_template(:show)
     end
   end
 
