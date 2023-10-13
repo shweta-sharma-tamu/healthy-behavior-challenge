714a646da9b602969d26a6d7f65f4f01d5cebad4
shweta-sharma-tamu <143050103+shweta-sharma-tamu@users.noreply.github.com>
2023-10-13 13:58:17 -0500

    Fixed cucumber tests

--- features/step_definitions/add_trainees_steps.rb
+++ features/step_definitions/add_trainees_steps.rb
@@ -1,15 +1,14 @@
 Given("I am an instructor trying to add trainees to a challenge") do
-    user = User.create(email: 'instructor2@example.com', password: 'abcdef', user_type: "Instructor")
+    user = User.create!(email: 'instructor00@example.com', password: 'abcdef', user_type: "Instructor")
     instructor = Instructor.create(user_id: user.id, first_name: 'John', last_name: 'Doe')
 
-    user_2 = User.create(email: 'trainee1@example.com', password: 'abcdefg', user_type: "Trainee")
+    user_2 = User.create!(email: 'trainee00@example.com', password: 'abcdefg', user_type: "Trainee")
+    user_3 = User.create!(email: 'trainee01@example.com', password: 'abcdefgh', user_type: "Trainee")
+    user_4 = User.create!(email: 'trainee02@example.com', password: 'abcdefgi', user_type: "Trainee")
 
-    user_3 = User.create(email: 'trainee2@example.com', password: 'abcdefgh', user_type: "Trainee")
-    user_4 = User.create(email: 'trainee3@example.com', password: 'abcdefgh', user_type: "Trainee")
-
-    trainee_1 = Trainee.create(full_name:'Trainee1 Name', height:1, weight:1, user_id:2)
-    trainee_2 = Trainee.create(full_name:'Trainee2 Name', height:2, weight:2, user_id:3)
-    trainee_3 = Trainee.create(full_name:'Trainee3 Name', height:2, weight:2, user_id:4)
+    trainee_1 = Trainee.create(full_name:'Trainee1 Name', height:1, weight:1, user_id:user_2.id)
+    trainee_2 = Trainee.create(full_name:'Trainee2 Name', height:2, weight:2, user_id:user_3.id)
+    trainee_3 = Trainee.create(full_name:'Trainee3 Name', height:2, weight:2, user_id:user_4.id)
   
     # Use Capybara to log in as the user
     visit root_path
@@ -38,21 +37,12 @@ Given("I am an instructor trying to add trainees to a challenge") do
   
   Then("I select a trainee and add it to {string}")  do |challenge_name|
 
-    # Extract the CSRF token from the page's meta tags
-    #csrf_token = find("meta[name=csrf-token]")[:content]
-    # challenge = Challenge.find_by(name: challenge_name)
-    # trainee_2 = Trainee.find_by(full_name:'Trainee2 Name')
-    # page.driver.post update_trainees_challenge_path(challenge.id), {trainee_ids: [trainee_2.id], headers: { "HTTP_X_CSRF_TOKEN" => csrf_token }  }
-
     challenge = Challenge.find_by(name: challenge_name)
     trainee_2 = Trainee.find_by(full_name:'Trainee2 Name')
 
     post update_trainees_challenge_path(challenge.id, {trainee_ids: [trainee_2.id]}) # Assuming this is a POST request
     fill_in "traineeSearch", with: trainee_2.full_name # Assuming this is the input field for trainee_ids
     
-
-    # Optionally, you can also wait for the form to be submitted (if using AJAX)
-   #expect(page).to have_text("Trainees were successfully added to the challenge.")
 
   end
 
