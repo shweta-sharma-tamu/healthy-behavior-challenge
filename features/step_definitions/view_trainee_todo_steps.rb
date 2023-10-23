a22176c091a00cad1ed5bc360221e60b4a09c824
Satya Bhavsar <satyabhavsar@tamu.edu>
2023-10-22 21:21:01 -0500

    Cucumber tests for view trainee todolist for instructor

--- features/step_definitions/view_trainee_todo_steps.rb
+++ features/step_definitions/view_trainee_todo_steps.rb
@@ -1,47 +1,109 @@
-
-
 Given('I am an instructor with a trainee {string} and following challenges:') do |string, table|
     user = User.create!(email: 'testInstructor@gmail.com', password: 'abcdef', user_type: "Instructor")
-    instructor = Instructor.create(user: user, first_name: 'John', last_name: 'Doe')
-    user_trainee = User.create!(email: 'testTrainee@gmail.com', password: 'abcdef', user_type: "Trainee")
-    trainee = Trainee.create(user: user_type, first_name: string, last_name: 'Doe') 
+    trainee_user = User.create!(email: 'testTrainee3@gmail.com', password: 'abcdef', user_type: "Trainee")
+    instructor = Instructor.create!(user: user, first_name: 'John', last_name: 'Doe')
+    trainee = Trainee.create!(user: trainee_user, full_name: string, height: 11, weight: 11)
     table.hashes.each do |challenge|
-        Challenge.create!(name: challenge['name'], startDate: challenge['startDate'], endDate: challenge['endDate'], instructor: instructor, tasks_attributes: {
+        challenge_create = Challenge.create!(name: challenge['name'], startDate: challenge['startDate'], endDate: challenge['endDate'], instructor: instructor, tasks_attributes: {
             '0' => { taskName: 'Task 1' },
-            '1' => { taskName: 'Task 1' }  # Use a different name for the second task
+            '1' => { taskName: 'Task 2' }  
           })
-     end
+        challenge_create.trainees << Trainee.where(id: trainee.id)
+        tasks = challenge_create.tasks
+        tasks.each do |task|
+            (challenge_create.startDate..challenge_create.endDate).each do |date|
+              tasktodo = TodolistTask.new(
+                task: task,
+                trainee: trainee,      
+                challenge: @challenge, 
+                date: date            
+              )
+                tasktodo.save   
+            end
+        end           
     end
+end
     
-  Given('I am an instructor with a trainee {string} and an Ongoing Challenge {string}') do |string, string2|
-    pending # Write code here that turns the phrase above into concrete actions
-  end
+Given('I am an instructor with a trainee {string} and an Ongoing Challenge {string}') do |string, string2|
+    user = User.create!(email: 'testInstructor@gmail.com', password: 'abcdef', user_type: "Instructor")
+    trainee_user = User.create!(email: 'testTrainee2@gmail.com', password: 'abcdef', user_type: "Trainee")
+    instructor = Instructor.create!(user: user, first_name: 'John', last_name: 'Doe')
+    trainee = Trainee.create!(user: trainee_user, full_name: string, height: 11, weight: 11)
+    puts trainee.id
+    challenge_create = Challenge.create!(name: string2, startDate: Date.today - 2, endDate: Date.today + 2, instructor: instructor, tasks_attributes: {
+            '0' => { taskName: 'Task 1' },
+            '1' => { taskName: 'Task 2' }  
+          })
+    challenge_create.trainees << Trainee.where(id: trainee.id)
+    tasks = challenge_create.tasks
+    tasks.each do |task|
+        (challenge_create.startDate..challenge_create.endDate).each do |date|
+            tasktodo = TodolistTask.new(
+            task: task,
+            trainee: trainee,      
+            challenge: @challenge, 
+            date: date            
+            )
+            tasktodo.save  
+        end 
+    end           
+end
   
   Given('I am an instructor with a trainee {string} and a Future Challenge {string} with start date of tomorrow') do |string, string2|
-    pending # Write code here that turns the phrase above into concrete actions
+    user = User.create!(email: 'testInstructor@gmail.com', password: 'abcdef', user_type: "Instructor")
+    trainee_user = User.create!(email: 'testTrainee1@gmail.com', password: 'abcdef', user_type: "Trainee")
+    instructor = Instructor.create!(user: user, first_name: 'John', last_name: 'Doe')
+    trainee = Trainee.create!(user: trainee_user, full_name: string, height: 11, weight: 11)
+    puts trainee.id
+    challenge_create = Challenge.create!(name: string2, startDate: Date.today + 2, endDate: Date.today + 4, instructor: instructor, tasks_attributes: {
+            '0' => { taskName: 'Task 1' },
+            '1' => { taskName: 'Task 2' }  
+          })
+    challenge_create.trainees << Trainee.where(id: trainee.id)
+    tasks = challenge_create.tasks
+    tasks.each do |task|
+        (challenge_create.startDate..challenge_create.endDate).each do |date|
+            tasktodo = TodolistTask.new(
+            task: task,
+            trainee: trainee,      
+            challenge: @challenge, 
+            date: date            
+            )
+            tasktodo.save  
+        end 
+    end           
   end
   
 
-Given('I click on challenge {string}') do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-end
-
-Given('I click on trainee {string}') do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-end
-
-Given('The trainee is registered for an Ongoing Challenge {string}') do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-end
 
 Then('I should see Current Date') do
-    pending # Write code here that turns the phrase above into concrete actions
+    date_picker_input = find('#datePicker')
+  
+    actual_date = date_picker_input.value
+  
+    expect(actual_date).to eq(Date.today)
+  end
+
+Then('I should see {string} date') do |date|
+    date_picker_input = find('#datePicker')
+  
+    actual_date = date_picker_input.value
+  
+    expect(page).to have_content(date)
 end
 
-Given('The trainee is registered for a Future Challenge {string} with start date of tomorrow') do |string|
-    pending # Write code here that turns the phrase above into concrete actions
-end
+
 
 Then('I should see tomorrows date') do
-    pending # Write code here that turns the phrase above into concrete actions
+    date_picker_input = find('#datePicker')
+  
+    actual_date = date_picker_input.value
+  
+    expect(actual_date).to eq(Date.today+1)
+end  
+
+Given('I follow View TodoList for {string}') do |string|
+    trainee = Trainee.find_by(full_name: 'TestTrainee')
+    challenge = Challenge.find_by(name: 'Challenge1')
+    visit show_challenge_trainee_path(challenge_id: challenge.id, trainee_id: trainee.id)
 end
---
+++ test/factories/trainee.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+    factory :trainee do
+      full_name { 'Trainee' }
+    end
+  end
