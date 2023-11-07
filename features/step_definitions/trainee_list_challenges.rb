2115a160b63651017fb671696933c6124fa6a7d5
Satya Bhavsar <satyabhavsar@tamu.edu>
2023-11-06 22:07:05 -0600

    Add cucumber tests for list trainee challenges

---
+++ features/step_definitions/trainee_list_challenges.rb
@@ -0,0 +1,20 @@
+Given('I am a trainee with following challenges:') do |table|
+    user_trainee = User.create!(email: 'testTrainee@gmail.com', password: 'abcdef',user_type: "trainee")
+    @trainee = Trainee.create!(full_name: 'trainee1', height: 165, weight: 85, user_id: user_trainee.id)
+
+    user = User.create!(email: 'testInstructor@gmail.com', password: 'abcdef', user_type: "Instructor")
+    @instructor = Instructor.create!(user: user, first_name: 'John', last_name: 'Doe')
+
+    table.hashes.each do |challenge|
+        challenge = Challenge.create!(name: challenge['name'], startDate: challenge['startDate'], endDate: challenge['endDate'], instructor: @instructor, tasks_attributes: {
+            '0' => { taskName: 'Task 1' },
+            '1' => { taskName: 'Task 1' }  
+          })
+        challenge.trainees << Trainee.where(id: @trainee.id)
+     end
+
+end
+  
+Given('I visit {string}') do |string|
+    visit show_challenges_path
+end
